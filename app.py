bb68dcad1ea200f177e97557eb781608e7819e56
Umang Jayswal <128044098+UmangAj@users.noreply.github.com>
2023-06-24 17:59:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ ftype1 = st.selectbox("**Select Fuel Type :** ", ftype, index=0)
 cbody1 = st.selectbox("**Select Car Body Type :** ", cbody, index=0)
 dwheel1 = st.selectbox("**Select Drive Wheel :** ", dwheel, index=0)
 etype1 = st.selectbox("**Select Engine Type :** ", etype, index=0)
-hp1 = st.number_input("**Enter horsepower :**")
+hp1 = st.text_input("**Enter horsepower :**")
 if st.button("Predict"):
     prediction = int(model.predict(pd.DataFrame([[cname1, ftype1, cbody1, dwheel1, etype1, hp1]],
                                             columns=["carname", "fueltype", "carbody", "drivewheel", "enginetype","horsepower"])))
